3bd4786f33be4fa1e2616f8354bce30920caedaa
Brant Shapka <bshapka@gmail.com>
2021-06-27 13:39:05 -0700

    added density insert to initialize_config.sql, and ran corresponding insert statement

--- initialize_config.sql
+++ initialize_config.sql
@@ -1,2 +1,3 @@
 insert into config (key, value) values ("MAX_X_COORDINATE", "100");
 insert into config (key, value) values ("MAX_Y_COORDINATE", "50");
+insert into config (key, value) values ("DENSITY", "0.075");
