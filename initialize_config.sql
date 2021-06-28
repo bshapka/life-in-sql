09b3f3b035acf75f1e663d201e1f2eddceaa5a48
Brant Shapka <bshapka@gmail.com>
2021-06-27 17:31:38 -0700

    added delete statements to initialize scripts

--- initialize_config.sql
+++ initialize_config.sql
@@ -1,3 +1,4 @@
+delete from config;
 insert into config (key, value) values ("MAX_X_COORDINATE", "100");
 insert into config (key, value) values ("MAX_Y_COORDINATE", "50");
 insert into config (key, value) values ("DENSITY", "0.075");
--- initialize_state.sql
+++ initialize_state.sql
@@ -1,24 +1,25 @@
-with recursive
-    initial_state(n, x_coordinate, y_coordinate, state_size) as (
-        values(
-            0,
-            abs(random() % cast((select value from config where key = 'MAX_X_COORDINATE') as integer)),
-            abs(random() % cast((select value from config where key = 'MAX_Y_COORDINATE') as integer)),
-            cast((select value from config where key = 'MAX_X_COORDINATE') as integer) *
-            cast((select value from config where key = 'MAX_Y_COORDINATE') as integer) *
-            cast((select value from config where key = 'DENSITY') as real)
-        )
-        union all
-        select
-            n + 1,
-            abs(random() % cast((select value from config where key = 'MAX_X_COORDINATE') as integer)),
-            abs(random() % cast((select value from config where key = 'MAX_Y_COORDINATE') as integer)),
-            state_size
-        from
-            initial_state
-        where
-            n + 1 < state_size * 2
+delete from state;
+
+with recursive initial_state(n, x_coordinate, y_coordinate, state_size) as (
+    values(
+        0,
+        abs(random() % cast((select value from config where key = 'MAX_X_COORDINATE') as integer)),
+        abs(random() % cast((select value from config where key = 'MAX_Y_COORDINATE') as integer)),
+        cast((select value from config where key = 'MAX_X_COORDINATE') as integer) *
+        cast((select value from config where key = 'MAX_Y_COORDINATE') as integer) *
+        cast((select value from config where key = 'DENSITY') as real)
     )
+    union all
+    select
+        n + 1,
+        abs(random() % cast((select value from config where key = 'MAX_X_COORDINATE') as integer)),
+        abs(random() % cast((select value from config where key = 'MAX_Y_COORDINATE') as integer)),
+        state_size
+    from
+        initial_state
+    where
+        n + 1 < state_size * 2
+)
 insert into
     state
 select distinct
