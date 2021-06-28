036487850ebb9660e2b6d7e0b56d44b88649bed2
Brant Shapka <bshapka@gmail.com>
2021-06-28 14:32:05 -0700

    changed MAX_X_COORDINATE to WIDTH and MAX_Y_COORDINATE to HEIGHT

--- initialize_config.sql
+++ initialize_config.sql
@@ -2,18 +2,17 @@
 --
 -- Descriptions:
 --
--- MAX_X_COORDINATE: The largest x coordinate possible for rendering the state. Units are characters.
--- MAX_Y_COORDINATE: The largest y coordinate possible for rendering the state Units are characters.
--- DENSITY: The fraction of cells in the initial state of area (MAX_X_COORDINATE + 1) * (MAX_Y_COORDINATE + 1) that are
--- live
+-- WIDTH: The width (horizontal dimension) of the rectangular area for rendering state. Units are characters.
+-- HEIGHT: The height (vertical dimension) of the rectangular area for rendering state. Units are characters.
+-- DENSITY: The fraction of cells in the initial state of area WIDTH * HEIGHT that are live
 --
 -- Additional Notes:
 --
 -- SQL lacks the graphical capabilities of other languages for obvious reasons, so approaches for rendering the state of
 -- the game are somewhat limited. However, rendering the state via SQL is not unlike other languages in that the
--- dimensions of the plane to be rendered must be specified, hence the desire for storing MAX_X|Y_COORDINATE.
+-- dimensions of the plane to be rendered must be specified, hence the desire for storing WIDTH and HEIGHT.
 --
 delete from config;
-insert into config (key, value) values ("MAX_X_COORDINATE", "100");
-insert into config (key, value) values ("MAX_Y_COORDINATE", "50");
+insert into config (key, value) values ("WIDTH", "100");
+insert into config (key, value) values ("HEIGHT", "50");
 insert into config (key, value) values ("DENSITY", "0.075");
